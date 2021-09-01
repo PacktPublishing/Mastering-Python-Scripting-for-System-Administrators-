bb3dd0fbe1c7855df75e85691aee4c7259980a24
beam2896 <pranjal.gp99@gmail.com>
2021-09-01 11:41:02 +0530

    added input combinations for file/directory

--- Chapter05/os_remove_file_directory.py
+++ Chapter05/os_remove_file_directory.py
@@ -1,7 +1,11 @@
 import os
 
-os.remove('sample.txt')
-print("File removed successfully")
-
-os.rmdir('work1')
-print("Directory removed successfully")
+inp=input("enter f for deleting a file, d for a directory")
+if inp == "f":
+    os.remove('sample.txt')
+    print("File removed successfully")
+elif inp == "d":
+    os.rmdir('work1')
+    print("Directory removed successfully")
+else:
+    print("wrong input")
