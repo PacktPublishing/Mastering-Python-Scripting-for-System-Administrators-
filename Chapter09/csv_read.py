3a683185ff7c893e8b783cce4416251470367580
beam2896 <pranjal.gp99@gmail.com>
2021-09-07 11:07:39 +0530

    file change

--- Chapter09/csv_read.py
+++ Chapter09/csv_read.py
@@ -2,7 +2,7 @@ import csv
 
 csv_file = open('test.csv', 'r')
 with csv_file:
-read_csv = csv.reader(csv_file)
-    	for row in read_csv:
-      		print(row)
+    read_csv = csv.reader(csv_file)
+    for row in read_csv:
+        print(row)
 
